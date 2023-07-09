c02e2feab93248b280d480ffff64b5a78bb6a125
Plaxidia Hove <plaxidiahove@gmail.com>
2023-07-10 02:10:45 +0300

    practise on hooks

--- src/theapp/home.js
+++ src/theapp/home.js
@@ -1,18 +1,25 @@
-const Home = () => {
-    const handleclick =(e) =>{
-        console.log("hello,P ",e);
-    }
-    const handleclickAgain =(name, e) => {
-        console.log("hello",+ name,e.target)
+import {useState} from 'react';
 
+const Home = () => {
+    //let name = "Plaxie";
+    const [name ,setName] = useState('Musaigwa');
+    const [age ,setAge] =useState (23);
+
+
+    const handleclick =() =>{
+      setName("hove");
+      setAge(24);
     }
     return ( 
         <div className=" home">
             <h2> HomePage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick = {handleclick}> Click here</button>
-            <button onClick = {(e) =>handleclickAgain ('Plaxie',e)} >Click here again</button>
         </div>
      );
 }
  
 export default Home;
+/*
+ <button onClick = {(e) =>handleclickAgain ('Plaxie',e)} >Click here again</button>
+ */
