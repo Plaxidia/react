7e149708ef1028e811739ab7acd2d9ecf55d31d1
Plaxidia Hove <plaxidiahove@gmail.com>
2023-07-23 01:41:17 +0300

    conditional loading message

--- src/theapp/bloglist.js
+++ src/theapp/bloglist.js
@@ -1,17 +1,13 @@
-const Bloglist = ({blogs,title,handleDelete}) => {
+const Bloglist = ({blogs,title}) => {
 
     //const blogs = props.blogs;
     //console.log(props,blogs);
-    
-    //console.log("blogs",blogs);
-
     return (
         <div className="blog-list">
             {blogs.map((blog) =>(
             <div className="blog-preview" key ={blog.id}>
                 <h2> { blog.title }</h2>
                 <p> Written by { blog.author} </p>
-                <button onClick ={() => handleDelete(blog.id)}> Delete blog</button>
             </div>
 
            ))}
