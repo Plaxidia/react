45afb410106c499543dde9c9aae3640b69867b13
Plaxidia Hove <plaxidiahove@gmail.com>
2023-07-23 02:32:44 +0300

    handling fetch error

--- src/theapp/home.js
+++ src/theapp/home.js
@@ -5,22 +5,38 @@ import Bloglist from './bloglist';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPendeing ] = useState(true);
+    const [error , setError] =useState(null);
 
     useEffect(() => { 
         setTimeout (() => {
             fetch(" http://localhost:3000/blogs")
             .then(res => {
+                console.log(res);
+            if(!res.ok)
+            {
+                throw Error ("Could not fetch the data");
+
+            }
+        
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsPendeing(false);
+                setError(null);
             })
-        },2000)
+            .catch(err => {
+                setIsPendeing(false);
+                setError(err.message);
+                //console.log(err.message);
+                        })
+            
+        },2000);
    
     }, []);
     return (
         <div className=" home">
+            {error && <div> {error}</div>}
             {isPending && <div> Loading.....</div>}
             {blogs && <Bloglist blogs = {blogs} title = {"All blogs"}/>}
         </div>
